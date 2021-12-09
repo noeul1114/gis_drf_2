6df07993721278d3fd4832829a3ef75c83221b0e
noeul1114 <parkhyongsok@naver.com>
2021-12-09 13:24:13 +0900

    hello_world view update

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view()
+def hello_world(request):
+    return Response({'message': 'Hello World!'})
