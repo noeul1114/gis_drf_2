310d2ed076b9f334261ab8ecba189cf36e799113
noeul1114 <parkhyongsok@naver.com>
2021-12-09 13:49:43 +0900

    hello_world_template update

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, hello_world_template
 
 app_name = 'accountapp'
 
 urlpatterns = [
+    # UI를 보기 위한 부분
+    path('hello_world_template/', hello_world_template, name='hello_world_template'),
+    # 로직 처리 위한 부분
     path('hello_world/', hello_world, name='hello_world'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -5,6 +5,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
+# UI 설정부분
+def hello_world_template(request):
+    return render(request, 'accountapp/hello_world.html')
+
+
+
+# 로직 처리부분
 @api_view()
 def hello_world(request):
     return Response({'message': 'Hello World!'})
