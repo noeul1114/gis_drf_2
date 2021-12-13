c6c587b7cd23e66c53498e92b0166b8b7122b6c2
noeul1114 <parkhyongsok@naver.com>
2021-12-13 14:50:07 +0900

    login cookie update

--- accountapp/static/js/login.js
+++ accountapp/static/js/login.js
@@ -1,3 +1,27 @@
+function setCookie(name, value, options = {}) {
+
+  options = {
+    path: '/',
+    // 필요한 경우, 옵션 기본값을 설정할 수도 있습니다.
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
 
 function send_input() {
     axios.post('/accounts/login/', {
@@ -12,7 +36,7 @@ function send_input() {
                 = "<div class='btn btn-primary rounded-pill px-5'>로그인이 성공했습니다</div>";
 
             // Token 수령 후 쿠키 생성
-            document.cookie = "drf_token=Token " + response.data['token'];
+            setCookie('drf_token', 'Token ' + response.data['token']);
 
         })
         .catch(function (error) {
---
+++ accountapp/static/js/token_check.js
@@ -0,0 +1,17 @@
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+
+var check = getCookie('drf_token');
+
+if (check !== undefined) {
+    document.getElementById('signup_button').innerHTML = "";
+    document.getElementById('login_button').innerHTML =
+        "<a href=\"/accounts/logout/\">\n" +
+        "                Logout\n" +
+        "            </a>";
+}
