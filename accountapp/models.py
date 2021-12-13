f3107d93a3e8154b864de7ac649be116fcce0cc7
noeul1114 <parkhyongsok@naver.com>
2021-12-13 11:08:38 +0900

    Token signal generation update

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,8 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class NewModel(models.Model):
     text = models.CharField(max_length=255, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
