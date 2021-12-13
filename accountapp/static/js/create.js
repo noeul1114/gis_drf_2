dab82d64aebb036f3478b0bb2286eeba83e68ff4
noeul1114 <parkhyongsok@naver.com>
2021-12-13 09:57:22 +0900

    DRF account create javascript update 2

--- accountapp/static/js/create.js
+++ accountapp/static/js/create.js
@@ -1,23 +1,22 @@
 
 function send_input() {
-    axios.post('/accounts/hello_world/', {
-        input_data: document.getElementById('input_data').value,
+    axios.post('/accounts/create/', {
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value,
     })
         .then(function (response) {
-            // POST 요청이 성공했을 경우 어떤 javascript 가 구동되어야 하는지
+            // 성공했을 경우
             console.log(response);
 
-            document.getElementById('text').innerHTML = response.data['text'];
-            document.getElementById('new_model_created_at').innerHTML
-                = response.data['created_at'];
-
-            document.getElementById('new_model_list').innerHTML
-                += "<h5>" + response.data['text'] + "</h5>"
-            document.getElementById('new_model_list').innerHTML
-                += "<p>" + response.data['created_at'] + "</p>"
+            document.getElementById('alert_box').innerHTML
+                = "<div class='btn btn-primary rounded-pill px-5'>가입이 성공했습니다</div>"
 
         })
         .catch(function (error) {
+            // 실패했을 경우
             console.log(error);
+
+            document.getElementById('alert_box').innerHTML
+                = "<div class='btn btn-danger rounded-pill px-5'>가입이 실패했습니다</div>"
         });
 }
