4603b64e7a1c245efa9391d8b015d71acd1ce9bc
noeul1114 <parkhyongsok@naver.com>
2021-12-13 11:42:48 +0900

    Account create view fixed

--- accountapp/serializers.py
+++ accountapp/serializers.py
@@ -11,3 +11,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
