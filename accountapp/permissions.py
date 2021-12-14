6397ff9e148ae175b85019c004bfd242b8cce0c6
noeul1114 <parkhyongsok@naver.com>
2021-12-14 16:49:24 +0900

    RUD is owner permission update

--- accountapp/permissions.py
+++ accountapp/permissions.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import User
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
 
 class IsOwner(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return obj == request.user
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return obj == request.user
