0e92c6c2059568f5a6b9ae58ddd89c2354aecf73
noeul1114 <parkhyongsok@naver.com>
2021-12-14 11:28:16 +0900

    Update API template javascript update 2

--- accountapp/static/js/update.js
+++ accountapp/static/js/update.js
@@ -16,3 +16,26 @@ function initialize(pk) {
             // always executed
         });
 }
+
+function update_account(pk) {
+    axios.patch('/accounts/update/' + pk, {
+        username: document.getElementById('username').value,
+        email: document.getElementById('email').value,
+    })
+        .then(function (response) {
+            // handle success
+            console.log(response);
+
+            window.location.href = '/accounts/retrieve_template/' + pk;
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+
+            document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>업데이트에 실패했습니다.</div>"
+        })
+        .then(function () {
+            // always executed
+        });
+}
