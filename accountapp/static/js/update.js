04c2a44a4a176f716f64018b8ef0854fd0116b66
noeul1114 <parkhyongsok@naver.com>
2021-12-14 15:10:08 +0900

    update API error code update

--- accountapp/static/js/update.js
+++ accountapp/static/js/update.js
@@ -32,8 +32,17 @@ function update_account(pk) {
             // handle error
             console.log(error);
 
-            document.getElementById('alert_box').innerHTML
+            if (error.response.status === 401) {
+                document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>인증 정보가 없습니다.</div>"
+            } else if (error.response.status === 403) {
+                document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>권한이 없습니다.</div>"
+            } else {
+                document.getElementById('alert_box').innerHTML
                     = "<div class='btn btn-danger rounded-pill px-5'>업데이트에 실패했습니다.</div>"
+            }
+
         })
         .then(function () {
             // always executed
