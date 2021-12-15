56c8d177a2c895e5bf15cf5384407d354a7d488f
noeul1114 <parkhyongsok@naver.com>
2021-12-15 14:50:01 +0900

    Profile Create template update

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateAPIView
+from profileapp.views import ProfileCreateAPIView, ProfileCreateTemplateView
+
+app_name = 'profileapp'
 
 urlpatterns = [
+    path('create_template/', ProfileCreateTemplateView.as_view(), name='create_template'),
+
     path('', ProfileCreateAPIView.as_view(), name='create'),
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from profileapp.models import Profile
 from profileapp.serializers import ProfileSerializer
+
+
+class ProfileCreateTemplateView(TemplateView):
+    template_name = 'profileapp/create.html'
 
 
 class ProfileCreateAPIView(CreateAPIView):
