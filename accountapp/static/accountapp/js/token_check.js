809bb881b0ab9746162606ed8c76e50712904228
noeul1114 <parkhyongsok@naver.com>
2021-12-16 11:54:32 +0900

    header nav button mypage update

--- accountapp/static/accountapp/js/token_check.js
+++ accountapp/static/accountapp/js/token_check.js
@@ -5,14 +5,28 @@ function getCookie(name) {
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
+var user_id;
+
+axios({
+    method: 'get',
+    url: '/accounts/token/',
+    headers: {
+        Authorization: decodeURIComponent(getCookie('drf_token'))
+    }
+})
+.then(function (response) {
+    // 성공했을 경우
+    user_id = response.data['id'];
+    document.getElementById('signup_button').innerHTML =
+    "<a href=\"/accounts/retrieve_template/" + user_id + "\">\n" +
+    "                MyPage\n" +
+    "            </a>";
+})
+
 
 var check = getCookie('drf_token');
 
 if (check !== undefined) {
-    document.getElementById('signup_button').innerHTML =
-        "<a href=\"/accounts/retrieve_template/\">\n" +
-        "                MyPage\n" +
-        "            </a>";
     document.getElementById('login_button').innerHTML =
         "<a href=\"/accounts/logout_template/\">\n" +
         "                Logout\n" +
