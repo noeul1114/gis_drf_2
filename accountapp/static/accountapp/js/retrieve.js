cf986f8e1f7c0bb7efb9ad37401c35ef58a14c5b
noeul1114 <parkhyongsok@naver.com>
2021-12-16 16:40:15 +0900

    account retrieve page update

--- accountapp/static/accountapp/js/retrieve.js
+++ accountapp/static/accountapp/js/retrieve.js
@@ -1,5 +1,18 @@
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 function initialize(pk) {
-    axios.get('/accounts/' + pk)
+    axios({
+        method: 'get',
+        url: '/accounts/' + pk,
+        headers: {
+            Authorization: decodeURIComponent(getCookie('drf_token')),
+        }
+    })
         .then(function (response) {
             // handle success
             console.log(response);
@@ -14,6 +27,21 @@ function initialize(pk) {
             } else {
                 document.getElementById('image').style.display = 'none';
             }
+
+            var user_id = response.data['id'];
+            var profile_id = response.data['profile']['id'];
+
+            if (response.data['is_page_owner'] === "True") {
+                document.getElementById('owner_section').innerHTML =
+                    "<div>" +
+                    "<a href='/accounts/update_template/" + user_id + "'>Update Account</a>" +
+                    "</div>" +
+                    "<div>" +
+                    "<a href='/profiles/update_template/" + profile_id + "'>Update Profile</a>" +
+                    "</div>";
+            }
+
+
 
         })
         .catch(function (error) {
