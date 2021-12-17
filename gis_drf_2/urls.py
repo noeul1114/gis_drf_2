0b73d8c5101c33aaf8aecf3ed99b51d0470ec2c0
noeul1114 <parkhyongsok@naver.com>
2021-12-17 09:15:51 +0900

    articleapp initialize

--- gis_drf_2/urls.py
+++ gis_drf_2/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
+    path('articles/', include('articleapp.urls')),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
