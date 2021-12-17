fe46226490be28d991d54076374b9bff0b15f8ed
noeul1114 <parkhyongsok@naver.com>
2021-12-17 09:52:04 +0900

    article serializer read only update

--- articleapp/serializers.py
+++ articleapp/serializers.py
@@ -6,4 +6,5 @@ from articleapp.models import Article
 class ArticleSerializer(ModelSerializer):
     class Meta:
         model = Article
-        fields = ['title', 'image', 'content']
+        fields = ['title', 'image', 'content', 'thumb', 'created_at']
+        read_only_fields = ['thumb', 'created_at']
