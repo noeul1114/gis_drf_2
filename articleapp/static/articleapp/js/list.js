91f57f97eb634ce1e66acadeb1203093777fad48
noeul1114 <parkhyongsok@naver.com>
2021-12-22 13:22:38 +0900

    article template view magicgrid error fixed

--- articleapp/static/articleapp/js/list.js
+++ articleapp/static/articleapp/js/list.js
@@ -18,15 +18,13 @@ function initialize() {
             console.log(response);
 
             for (let i=0; i < response.data['results'].length; i++) {
-                document.getElementById('article_list').innerHTML
+                document.getElementById('item' + i ).innerHTML
                     +=
-                    "<div>" +
                     "<a href=\"/articles/retrieve_template/" + response.data['results'][i]['id'] + "\">" +
                     "<img style=\"width: 100%; border-radius: 1rem;\"" +
                     " src=\"" + response.data['results'][i]['image'] + "\"" +
                     " alt=\"\">" +
-                    "</a>\"" +
-                    "</div>";
+                    "</a>";
             }
 
             // paginator 기반 page 객체 생성
