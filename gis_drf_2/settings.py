d2344205b57c78e917fc58355df415fa96986a4f
noeul1114 <parkhyongsok@naver.com>
2021-12-22 14:54:59 +0900

    DRF yasg update

--- gis_drf_2/settings.py
+++ gis_drf_2/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'accountapp',
     'profileapp',
     'articleapp',
+    'drf_yasg',
 ]
 
 REST_FRAMEWORK = {
--- gis_drf_2/urls.py
+++ gis_drf_2/urls.py
@@ -16,9 +16,30 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="GIS Django REST API Course",
+      default_version='v1',
+      description="광인사 12월 웹 어플리케이션 보충 API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="parkhyongsok@naver.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
+      name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('admin/', admin.site.urls),
     
