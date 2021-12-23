aa2c0ec4fa03fe2eea93fe99f5b731af609d90de
noeul1114 <parkhyongsok@naver.com>
2021-12-23 16:26:41 +0900

    Async task update 2

--- profileapp/models.py
+++ profileapp/models.py
@@ -7,6 +7,9 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
 
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from image_processing import thumbnail
 from profileapp.tasks import generate_thumbnail_celery_lag
 
@@ -21,13 +24,7 @@ class Profile(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        self.generate_thumbnail()
-        super().save(*args, **kwargs)
 
-    def generate_thumbnail(self):
-        if self.image:
-            output = generate_thumbnail_celery_lag.delay(self.image)
-
-            self.thumb = InMemoryUploadedFile(output, "ImageField", self.image.name,
-                                              'image/jpeg', sys.getsizeof(output), None)
+@receiver(post_save, sender=Profile)
+def async_generate_thumbnail(sender, instance=None, created=False, **kwargs):
+    generate_thumbnail_celery_lag.delay(instance.pk)
--- profileapp/tasks.py
+++ profileapp/tasks.py
@@ -1,15 +1,21 @@
+import sys
 import time
 from io import BytesIO
 
 from PIL import Image
 from celery import shared_task
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
 @shared_task
-def generate_thumbnail_celery_lag(input_image):
+def generate_thumbnail_celery_lag(profile_id):
+    from profileapp.models import Profile
+
+    target_profile = Profile.objects.get(pk=profile_id)
+
     time.sleep(10)
 
-    img = Image.open(input_image)
+    img = Image.open(target_profile.image)
 
     width, height = img.size
     ratio = height / width
@@ -22,4 +28,11 @@ def generate_thumbnail_celery_lag(input_image):
     img.save(output, format='JPEG', quality=95)
     output.seek(0)
 
-    return output
+    target_profile.thumb = InMemoryUploadedFile(output,
+                                                "ImageField",
+                                                target_profile.image.name,
+                                                'image/jpeg',
+                                                sys.getsizeof(output),
+                                                None)
+    target_profile.save()
+
