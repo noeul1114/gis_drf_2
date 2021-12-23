a1bf60f1f003c7bdeeb8870ff9d92cba68d15a67
noeul1114 <parkhyongsok@naver.com>
2021-12-23 11:01:13 +0900

    local / deploy settings separation

--- gis_drf_2/settings/base.py
+++ gis_drf_2/settings/base.py
@@ -20,29 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 
-local_env = open(os.path.join(BASE_DIR, '.env'))
-env_list = dict()
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -95,17 +72,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'gis_drf_2.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
---
+++ gis_drf_2/settings/deploy.py
@@ -0,0 +1,34 @@
+from .base import *
+
+
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.lstrip().rstrip()
+    file.close()
+    return secret
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': read_secret('MARIADB_USER'),
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
+        'HOST': 'mariadb',
+        'PORT': '3306',
+    }
+}
+
